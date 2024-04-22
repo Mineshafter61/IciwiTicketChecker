bd880c57528184a8e0ebd511ca367e32726e31c6
Mineshafter61 <yuhang.061@gmail.com>
2024-04-22 18:22:27 +0900

    boilerplate code

--- src/main/java/limaru/ticketchecker/SignActionTicketCheck.java
+++ src/main/java/limaru/ticketchecker/SignActionTicketCheck.java
@@ -1,27 +1,45 @@
 package limaru.ticketchecker;
 
+import org.bukkit.entity.Player;
+import com.bergerkiller.bukkit.common.entity.type.CommonMinecart;
+import com.bergerkiller.bukkit.tc.controller.MinecartGroup;
+import com.bergerkiller.bukkit.tc.controller.MinecartMember;
 import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.events.SignChangeActionEvent;
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
+import com.bergerkiller.bukkit.tc.signactions.SignActionType;
+
 
 public class SignActionTicketCheck extends SignAction {
 
     @Override
     public boolean match(SignActionEvent info) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'match'");
+        return info.isType("checker"); 
     }
 
     @Override
     public void execute(SignActionEvent info) {
-        // TODO Auto-generated method stub
+        if (info.getAction() == SignActionType.GROUP_ENTER) {
+            MinecartGroup group = info.getGroup();
+            for (MinecartMember<?> member : group) {
+                CommonMinecart<?> minecartEntity = member.getEntity();
+                minecartEntity.getPlayerPassengers().forEach(player -> {
+                    if (!checkTicket(player))
+                        minecartEntity.removePassenger(player);
+                });
+            }
+        }
         throw new UnsupportedOperationException("Unimplemented method 'execute'");
     }
 
     @Override
     public boolean build(SignChangeActionEvent event) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'build'");
+        return true;
     }
+    
 
+    public boolean checkTicket(Player player) {
+        // TODO: IMPORTANT
+        return false;
+    }
 }
